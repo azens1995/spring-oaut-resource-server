85a8db9b35bfb1657079518c0fa815afb5215be1
Eklak Dangaura <azens1995@gmail.com>
2023-09-17 12:38:26 +0545

    feat: add configuration and endpoint for resource server

--- src/main/java/dev/eklak/springresourceserver/config/ResourceServerConfig.java
+++ src/main/java/dev/eklak/springresourceserver/config/ResourceServerConfig.java
@@ -1,2 +1,10 @@
-package dev.eklak.springresourceserver.config;public class ResourceServerConfig {
+package dev.eklak.springresourceserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@Configuration
+@EnableResourceServer // Allows spring boot to configure what's required for our app to become resource server
+public class ResourceServerConfig {
+
 }
--- src/main/java/dev/eklak/springresourceserver/controllers/HelloController.java
+++ src/main/java/dev/eklak/springresourceserver/controllers/HelloController.java
@@ -1,2 +1,13 @@
-package dev.eklak.springresourceserver.controllers;public class HelloController {
+package dev.eklak.springresourceserver.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!!";
+    }
 }
